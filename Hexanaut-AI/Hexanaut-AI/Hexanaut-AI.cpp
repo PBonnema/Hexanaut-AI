2eb7b680a04ebd6e3c76b850ba0052b53bacb5e0
Peter Bonnema <peter.bonnema@infosupport.com>
2024-01-18 09:27:37 +0100

    Added SFML circle

--- Hexanaut-AI/Hexanaut-AI/Hexanaut-AI.cpp
+++ Hexanaut-AI/Hexanaut-AI/Hexanaut-AI.cpp
@@ -1,9 +1,24 @@
-// Hexanaut-AI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
